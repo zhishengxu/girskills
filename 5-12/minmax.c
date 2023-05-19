69d824d4fdaeb892aaa3d1b769f44049ed868f9f
Dingmeixiong <brongmx@sis.sh.cn>
2023-05-19 14:38:22 +0800

    xu

---
+++ 5-12/minmax.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+
+{
+    int num1, num2;
+
+    printf("输入两个数 ");
+    scanf("%d %d", &num1, &num2);
+    if(num1 > num2){
+	printf("%d\n", num1);}
+    else{
+//    if(num1 < num2){
+	printf("%d\n", num2);}
+
+return 0;
+}
+
